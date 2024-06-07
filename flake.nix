c0befd7db1b123a39a5944eeea543456d9a7efb6
Jinser Kafka <aimer@purejs.icu>
2024-06-07 13:59:26 +0800

    save my flake

--- flake.nix
+++ flake.nix
@@ -11,106 +11,125 @@
     };
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]
-    (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = { allowBroken = true; };
-        };
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { allowBroken = true; };
+          };
 
-        pythonWithPackages = pkgs.python3.withPackages (ps: [ps.sphinx ps.myst-parser ps.sphinx_rtd_theme ps.pip]);
+          pythonWithPackages = pkgs.python3.withPackages (ps: [ ps.sphinx ps.myst-parser ps.sphinx_rtd_theme ps.pip ]);
 
-        docs = pkgs.stdenv.mkDerivation {
-          name = "hls-docs";
-          src = pkgs.lib.sourceFilesBySuffices ./. [ ".py" ".rst" ".md" ".png" ".gif" ".svg" ".cabal" ];
-          buildInputs = [ pythonWithPackages ];
-          # -n gives warnings on missing link targets, -W makes warnings into errors
-          buildPhase = ''cd docs; sphinx-build -n -W . $out'';
-          dontInstall = true;
-        };
+          docs = pkgs.stdenv.mkDerivation {
+            name = "hls-docs";
+            src = pkgs.lib.sourceFilesBySuffices ./. [ ".py" ".rst" ".md" ".png" ".gif" ".svg" ".cabal" ];
+            buildInputs = [ pythonWithPackages ];
+            # -n gives warnings on missing link targets, -W makes warnings into errors
+            buildPhase = ''cd docs; sphinx-build -n -W . $out'';
+            dontInstall = true;
+          };
 
-        # Support of GenChangelogs.hs
-        gen-hls-changelogs = hpkgs: with pkgs;
-          let myGHC = hpkgs.ghcWithPackages (p: with p; [ github ]);
-          in pkgs.runCommand "gen-hls-changelogs" {
-            passAsFile = [ "text" ];
-            preferLocalBuild = true;
-            allowSubstitutes = false;
-            buildInputs = [ git myGHC ];
-          } ''
-            dest=$out/bin/gen-hls-changelogs
-            mkdir -p $out/bin
-            echo "#!${runtimeShell}" >> $dest
-            echo "${myGHC}/bin/runghc ${./GenChangelogs.hs}" >> $dest
-            chmod +x $dest
-          '';
+          hls-wrapper = pkgs.writeShellApplication {
+            name = "haskell-language-server-wrapper";
+            text = ''
+              export PATH=$HOME/life/project/hls-about/haskell-language-server/dist-newstyle/build/x86_64-linux/ghc-9.6.3/haskell-language-server-2.8.0.0/x/haskell-language-server/build/haskell-language-server:$PATH
+              ./dist-newstyle/build/x86_64-linux/ghc-9.6.3/haskell-language-server-2.8.0.0/x/haskell-language-server-wrapper/build/haskell-language-server-wrapper/haskell-language-server-wrapper "$@" 2> >(tee -a stderr.log >&2)
+            '';
+          };
 
-        mkDevShell = hpkgs: with pkgs; mkShell {
-          name = "haskell-language-server-dev-ghc${hpkgs.ghc.version}";
-          # For binary Haskell tools, we use the default nixpkgs GHC
-          # This removes a rebuild with a different GHC version
-          # The drawback of this approach is that our shell may pull two GHC
-          # version in scope.
-          buildInputs = [
-            # our compiling toolchain
-            hpkgs.ghc
-            pkgs.haskellPackages.cabal-install
-            # Dependencies needed to build some parts of hackage
-            gmp zlib ncurses
-            # Changelog tooling
-            (gen-hls-changelogs pkgs.haskellPackages)
-            # For the documentation
-            pythonWithPackages
-            # @guibou: I'm not sure this is needed.
-            hlint
-            (pkgs.haskell.lib.justStaticExecutables (pkgs.haskell.lib.dontCheck pkgs.haskellPackages.opentelemetry-extra))
-            capstone
-            # ormolu
-            stylish-haskell
-            pre-commit
+          # Support of GenChangelogs.hs
+          gen-hls-changelogs = hpkgs: with pkgs;
+            let myGHC = hpkgs.ghcWithPackages (p: with p; [ github ]);
+            in pkgs.runCommand "gen-hls-changelogs"
+              {
+                passAsFile = [ "text" ];
+                preferLocalBuild = true;
+                allowSubstitutes = false;
+                buildInputs = [ git myGHC ];
+              } ''
+              dest=$out/bin/gen-hls-changelogs
+              mkdir -p $out/bin
+              echo "#!${runtimeShell}" >> $dest
+              echo "${myGHC}/bin/runghc ${./GenChangelogs.hs}" >> $dest
+              chmod +x $dest
+            '';
+
+          mkDevShell = hpkgs: with pkgs; mkShell {
+            name = "haskell-language-server-dev-ghc${hpkgs.ghc.version}";
+            # For binary Haskell tools, we use the default nixpkgs GHC
+            # This removes a rebuild with a different GHC version
+            # The drawback of this approach is that our shell may pull two GHC
+            # version in scope.
+            buildInputs = [
+              nodejs
+              nixpkgs-fmt
+
+              hls-wrapper
+
+              # our compiling toolchain
+              hpkgs.ghc
+              pkgs.haskellPackages.cabal-install
+              # Dependencies needed to build some parts of hackage
+              gmp
+              zlib
+              ncurses
+              # Changelog tooling
+              (gen-hls-changelogs pkgs.haskellPackages)
+              # For the documentation
+              pythonWithPackages
+              # @guibou: I'm not sure this is needed.
+              hlint
+              (pkgs.haskell.lib.justStaticExecutables (pkgs.haskell.lib.dontCheck pkgs.haskellPackages.opentelemetry-extra))
+              capstone
+              # ormolu
+              stylish-haskell
+              pre-commit
             ] ++ lib.optionals (!stdenv.isDarwin)
-                   [ # tracy has a build problem on macos.
-                     tracy
-                   ]
-              ++ lib.optionals stdenv.isDarwin
+              [
+                # tracy has a build problem on macos.
+                tracy
+              ]
+            ++ lib.optionals stdenv.isDarwin
               (with darwin.apple_sdk.frameworks; [
                 Cocoa
                 CoreServices
               ]);
 
-          shellHook = ''
-            # @guibou: I'm not sure theses lines are needed
-            export LD_LIBRARY_PATH=${gmp}/lib:${zlib}/lib:${ncurses}/lib:${capstone}/lib
-            export DYLD_LIBRARY_PATH=${gmp}/lib:${zlib}/lib:${ncurses}/lib:${capstone}/lib
-            export PATH=$PATH:$HOME/.local/bin
+            shellHook = ''
+              # @guibou: I'm not sure theses lines are needed
+              export LD_LIBRARY_PATH=${gmp}/lib:${zlib}/lib:${ncurses}/lib:${capstone}/lib
+              export DYLD_LIBRARY_PATH=${gmp}/lib:${zlib}/lib:${ncurses}/lib:${capstone}/lib
+              export PATH=$PATH:$HOME/.local/bin
 
-            # Install pre-commit hook
-            pre-commit install
-          '';
-        };
+              # export PATH=$HOME/life/project/haskell-language-server/dist-newstyle/build/x86_64-linux/ghc-9.6.3/haskell-language-server-2.7.0.0/x/haskell-language-server/build/haskell-language-server:$PATH
+              # export PATH=$HOME/life/project/haskell-language-server/dist-newstyle/build/x86_64-linux/ghc-9.6.3/haskell-language-server-2.7.0.0/x/haskell-language-server-wrapper/build/haskell-language-server-wrapper:$PATH
 
-      in with pkgs; rec {
-        # Developement shell with only dev tools
-        devShells = {
-          default = mkDevShell pkgs.haskellPackages;
-          shell-ghc90 = mkDevShell pkgs.haskell.packages.ghc90;
-          shell-ghc92 = mkDevShell pkgs.haskell.packages.ghc92;
-          shell-ghc94 = mkDevShell pkgs.haskell.packages.ghc94;
-          shell-ghc96 = mkDevShell pkgs.haskell.packages.ghc96;
-          shell-ghc98 = mkDevShell pkgs.haskell.packages.ghc98;
-        };
+              # Install pre-commit hook
+              pre-commit install
+            '';
+          };
+        in
+        rec {
+          # Developement shell with only dev tools
+          devShells = {
+            default = mkDevShell pkgs.haskellPackages;
+            shell-ghc90 = mkDevShell pkgs.haskell.packages.ghc90;
+            shell-ghc92 = mkDevShell pkgs.haskell.packages.ghc92;
+            shell-ghc94 = mkDevShell pkgs.haskell.packages.ghc94;
+            shell-ghc96 = mkDevShell pkgs.haskell.packages.ghc96;
+            shell-ghc98 = mkDevShell pkgs.haskell.packages.ghc98;
+          };
 
-        packages = {
-          docs = docs;
-        };
+          packages = {
+            docs = docs;
+          };
 
-        # The attributes for the default shell and package changed in recent versions of Nix,
-        # these are here for backwards compatibility with the old versions.
-        devShell = devShells.default;
-      });
+          # The attributes for the default shell and package changed in recent versions of Nix,
+          # these are here for backwards compatibility with the old versions.
+          devShell = devShells.default;
+        });
 
   nixConfig = {
     extra-substituters = [
